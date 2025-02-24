ad05d0a66d9076dfce7e568cafbc50f5eb0b195c
ppaann <reg.ppan@gmail.com>
2025-02-24 12:14:51 -0800

    eslint fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -24,8 +24,8 @@ export default {
       },
       keyframes: {
         fadeInUp: {
-          '0%': { opacity: 0, transform: 'translateY("30px")' },
-          '100%': { opacity: 1, transform: 'translateY(0)' },
+          '0%': { opacity: '0', transform: 'translateY("30px")' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
         },
       },
     },
