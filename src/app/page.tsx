4624e35fb9f183f1335c43003c16ba8c35e27aee
ppaann <reg.ppan@gmail.com>
2025-04-03 07:46:01 -0700

    layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import { Hero } from '@/sections/Hero';
 import { Projects } from '@/sections/Projects';
 import { About } from '@/sections/About';
+import { Lab } from '@/sections/Lab';
 
 export default function Home() {
   return (
     <div className='flex flex-col gap-8 row-start-2 items-center sm:items-start'>
       <Hero />
       <Projects />
+      <Lab />
       <About />
     </div>
   );
--- src/sections/Lab.tsx
+++ src/sections/Lab.tsx
@@ -5,13 +5,13 @@ import { SectionHead } from '@/components/SectionHead';
 import React from 'react';
 export const Lab = () => {
   return (
-    <div className='flex flex-col items-center justify-center h-screen'>
+    <section className='flex flex-col items-center justify-center h-screen w-screen'>
       <SectionHead
         title='Lab/Experiments'
         header='My experimental projects'
         description='AI, Prototyping, prove of concept and more...'
       />
-      <div className='max-w-7xl w-full grid grid-cols-1 md:grid-cols-2 gap-8'>
+      <div className='max-w-7xl w-full grid grid-cols-1 md:grid-cols-2 gap-8 md:px-10'>
         <CardMini
           title=' Design-to-Code Automation Toolkit'
           description='A fully automated design-to-code pipeline that bridges Figma and React, AI-assisted component generation and continuous visual testing.'
@@ -25,6 +25,6 @@ export const Lab = () => {
           ]}
         />
       </div>
-    </div>
+    </section>
   );
 };
