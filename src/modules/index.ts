bc9bacdd87e6ddf863db53c0f076bb7ea7929937
ppaann <reg.ppan@gmail.com>
2025-04-09 07:32:01 -0700

    canvas responsive module index

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -2,3 +2,5 @@ export { default as ProjectSummary } from './ProjectSummary';
 export { default as ImageContainer } from './ImageContainer';
 export { default as LayoutToggle } from './controls/LayoutToggle';
 export { default as ThemeToggle } from './controls/ThemeToggle';
+
+export { default as CanvasParticles } from './canvas/CanvasWrapper';
