a52ef2d7bb818f4a469b0840331fda062c844f6a
ppaann <reg.ppan@gmail.com>
2025-04-26 18:20:55 -0700

    one more lab

--- src/components/CardMini/CardMini.tsx
+++ src/components/CardMini/CardMini.tsx
@@ -5,9 +5,15 @@ interface CardMiniProps {
   title: string;
   description: string;
   tools?: string[];
+  tags?: string[];
 }
 
-const CardMini: React.FC<CardMiniProps> = ({ title, description, tools }) => {
+const CardMini: React.FC<CardMiniProps> = ({
+  title,
+  description,
+  tools,
+  tags,
+}) => {
   return (
     <div className='card-gradient-border rounded-lg p-6 shadow-lg relative card-gradient-border transition duration-300 hover:shadow-glow-sm-cyan dark:hover:shadow-glow-md-purple cursor-pointer'>
       <div
@@ -32,12 +38,24 @@ const CardMini: React.FC<CardMiniProps> = ({ title, description, tools }) => {
           </div>
         )}
         <div className='flex space-x-2'>
-          <span className='text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-cyan-600 bg-cyan-200 dark:bg-cyan-800 dark:text-cyan-200'>
-            #AI
-          </span>
-          <span className='text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-purple-600 bg-purple-200 dark:bg-purple-800 dark:text-purple-200'>
-            #ComponentLibrary
-          </span>
+          {tags &&
+            tags.map((tag, index) => {
+              const tagColor =
+                index % 3 === 0
+                  ? 'text-gray-700 bg-gray-200 dark:bg-gray-800 dark:text-gray-200'
+                  : index % 3 === 1
+                  ? 'text-cyan-600 bg-cyan-200 dark:bg-cyan-800 dark:text-cyan-200'
+                  : 'text-purple-600 bg-purple-200 dark:bg-purple-800 dark:text-purple-200';
+
+              return (
+                <span
+                  key={index}
+                  className={`text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full ${tagColor}`}
+                >
+                  {tag}
+                </span>
+              );
+            })}
         </div>
       </div>
     </div>
--- src/sections/Lab.tsx
+++ src/sections/Lab.tsx
@@ -3,6 +3,27 @@ import { CardMini } from '@/components';
 import { SectionHead } from '@/components/SectionHead';
 import Link from 'next/link';
 
+const labProjects = [
+  {
+    id: '1',
+    title: 'Design-to-Code Automation Toolkit',
+    description:
+      'A fully automated design-to-code pipeline that bridges Figma and React, AI-assisted component generation and continuous visual testing.',
+    tools: ['Figma', 'React', 'AI', 'GitAction', 'Storybook', 'chromatic'],
+    link: '/lab/figma-to-react',
+    tags: ['#AI', '#ComponentLibrary', '#Figma'],
+  },
+  {
+    id: '2',
+    title: 'Realtime Stock Market Dashboard',
+    description:
+      'A real-time stock market dashboard that provides live updates on stock prices, add/remove symbol from watchlist. Responsive layout design make it mobile friendly.',
+    tools: ['React', 'WebSocket', 'Chart.js'],
+    link: 'https://stock-dashboard-amber.vercel.app/',
+    tags: ['#WebSocket', '#Chart.js', '#RestAPI'],
+  },
+];
+
 import React from 'react';
 export const Lab = () => {
   return (
@@ -13,20 +34,16 @@ export const Lab = () => {
         description='AI, Prototyping, prove of concept and more...'
       />
       <div className='max-w-7xl w-full grid grid-cols-1 md:grid-cols-2 gap-8 md:px-10 md:pt-5'>
-        <Link href='/lab/figma-to-react'>
-          <CardMini
-            title=' Design-to-Code Automation Toolkit'
-            description='A fully automated design-to-code pipeline that bridges Figma and React, AI-assisted component generation and continuous visual testing.'
-            tools={[
-              'Figma',
-              'React',
-              'AI',
-              'GitAction',
-              'Storybook',
-              'chromatic',
-            ]}
-          />
-        </Link>
+        {labProjects.map((project) => (
+          <Link key={project.id} href={project.link}>
+            <CardMini
+              title={project.title}
+              description={project.description}
+              tools={project.tools}
+              tags={project.tags}
+            />
+          </Link>
+        ))}
       </div>
     </section>
   );
